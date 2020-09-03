6648f7c3c95dc6d3fe5a764a10ee2351c5f13fe5
Mourad Bougarne <contact@mbougarne.me>
2020-09-04 00:53:53 +0100

    Create gmcolors.py

---
+++ gmcolors.py
@@ -0,0 +1,14 @@
+# This code by joeld from Stackoverflow
+# https://stackoverflow.com/questions/287871/how-to-print-colored-text-in-python
+# Thanks to them
+
+colors = {
+    "HEADER" : '\033[95m',
+    "OKBLUE" : '\033[94m',
+    "OKGREEN" : '\033[92m',
+    "WARNING" : '\033[93m',
+    "FAIL" : '\033[91m',
+    "ENDC" : '\033[0m',
+    "BOLD" : '\033[1m',
+    "UNDERLINE" : '\033[4m',
+}
